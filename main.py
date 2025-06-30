1181cf391a6808776b9f0ca28170fa86553015ef
kaedelily <svlibertini1998@gmail.com>
2025-06-30 05:05:25 -0400

    dt

--- main.py
+++ main.py
@@ -7,13 +7,19 @@ import pygame
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    pygame.time.Clock()
+
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        pygame.time.Clock.tick(60)
+        dt = pygame.time.Clock().tick(60) / 1000
+
 
 
     
