6036f276601b6bd0d2d9f96db0b8f57c90c97987
kaedelily <svlibertini1998@gmail.com>
2025-07-01 16:03:55 -0400

    0.0.8 collision

--- circleshape.py
+++ circleshape.py
@@ -20,3 +20,10 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # sub-classes must override
         pass
+    
+    def collision(self, other):
+        distance = self.radius + other.radius
+        if distance <= (self.radius + other.radius):
+            return True
+        else:
+            return False
